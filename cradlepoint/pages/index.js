d50c31b0f4acb9d4f302bfb0b3af38d4fdfb68fa
natashawong <natasha.wong10@gmail.com>
2021-09-19 22:25:06 -0700

    update components

--- cradlepoint/pages/index.js
+++ cradlepoint/pages/index.js
@@ -6,38 +6,17 @@ import Stack from '@mui/material/Stack';
 
 export default function Home() {
   const columns = [
-    { field: 'id', headerName: 'ID', width: 80 },
-    { field: 'firstName', headerName: 'First name', width: 200 },
-    { field: 'lastName', headerName: 'Last name', width: 200 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.getValue(params.id, 'firstName') || ''} ${
-          params.getValue(params.id, 'lastName') || ''
-        }`,
-    },
+    { field: 'device', headerName: 'Device', width: 200 },
+    { field: 'qty', headerName: 'Quantity', width: 150 },
+    { field: 'reqs', headerName: 'Requirements', width: 400 },
+    { field: 'hardware', headerName: 'Physical/Virtual', width: 200 },
   ];
 
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, device: 'CR4250', qty: 3, reqs: "", hardware: "Physical" },
+    { id: 2, device: 'E300', qty: 2, reqs: "", hardware: "Physical" },
+    { id: 3, device: 'Switch', qty: 1, reqs: "", hardware: "Physical" },
+    { id: 4, device: 'PC for clients (if possible)', qty: 1, reqs: "", hardware: "Virtual" },
   ];
 
   return (
@@ -48,11 +27,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <h1>Example menu bar [WIP]</h1>
+      <Stack direction="row" spacing={4} paddingBottom={1} borderBottom="1px solid">
+        <Button>HOME</Button>
+        <Button>TEST PLAN</Button>
+        <Button>BILL OF MATERIALS</Button>
+        <Button>REVIEW</Button>
+      </Stack>
+
       <h1>Example buttons - MUI/Buttons</h1>
       <Stack direction="row" spacing={2}>
         {/* styles will be cleaned up in actual implementation */}
         <Button variant="contained" style={{ borderRadius: 5, paddingTop: 10, paddingBottom: 10, paddingLeft: 20, paddingRight: 20 }} >Submit</Button>
         <Button variant="contained" disabled style={{ borderRadius: 5, paddingTop: 10, paddingBottom: 10, paddingLeft: 20, paddingRight: 20 }} >Submit</Button>
+
+        <Button variant="contained" style={{ borderRadius: 5, paddingTop: 10, paddingBottom: 10, paddingLeft: 20, paddingRight: 20 }} >Edit</Button>
+        <Button variant="contained" disabled style={{ borderRadius: 5, paddingTop: 10, paddingBottom: 10, paddingLeft: 20, paddingRight: 20 }} >Edit</Button>
       </Stack>
 
       <h1>Example table w/ pagination - MUI/Data-Grid</h1>
