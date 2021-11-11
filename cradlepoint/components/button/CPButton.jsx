c03d3d77681312eaf7244639ab33a7704fae6a93
Jkwok2018 <jessica.kwok19@gmail.com>
2021-11-11 12:34:27 -0800

    add btn that align horizontally instead of vertically

--- cradlepoint/components/button/CPButton.jsx
+++ cradlepoint/components/button/CPButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
 
-export default function CPButton(props) {
+function CPButton(props) {
     return (
         <Button 
             style={{borderRadius: 8, backgroundColor: "#FCAC1C", margin: 10, display: 'flex'}} 
@@ -17,6 +17,23 @@ export default function CPButton(props) {
     )
 }
 
+function HorizontalButton(props) {
+    return (
+        <Button 
+            style={{borderRadius: 8, backgroundColor: "#FCAC1C", margin: 10}} 
+            variant="contained" 
+            size="large"
+            onClick={props.onClick}
+        >
+            <div style={{padding: "10px"}}>
+            {props.text}
+            </div>
+        </Button>
+    )
+}
+
 Button.propTypes = {
     text: PropTypes.any
 }
+
+export {CPButton, HorizontalButton}; 
