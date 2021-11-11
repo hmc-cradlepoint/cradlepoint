633858e9241eb98f27a725acc0c622a1ef4b2068
Jkwok2018 <jessica.kwok19@gmail.com>
2021-11-11 12:36:19 -0800

    Added test plan details, can add new test case

---
+++ cradlepoint/pages/4TestPlanDetails.jsx
@@ -0,0 +1,220 @@
+import React, {useState} from 'react';
+import SplitScreen from '../components/baseScreen/SplitScreen';
+import { PlainTable } from '../components/tables/Table';
+import { makeStyles } from '@mui/styles';
+import { CPButton, HorizontalButton } from '../components/button/CPButton';
+import NewTestCaseModalInfo from './testCaseModals/newCaseModalInfo';
+import NewTestCaseModal from './testCaseModals/newCaseModal';
+import NewTestCaseModalClone from './testCaseModals/newCaseModalClone';
+import styles from '../styles/EngagementDetails.module.css';
+
+export default function TestPlanDetails() {
+
+    const useStyles = makeStyles({
+        root: {
+          '& .header': {
+            backgroundColor: '#FCAC1C',
+          },
+          '& .MuiDataGrid-iconSeparator': {
+            display: 'None'
+          },
+          '& .MuiDataGrid-columnHeader, .MuiDataGrid-cell': {
+            borderRight: `2px solid #f0f0f0`,
+          },
+        },
+      });
+    
+    const classes = useStyles();
+
+    const testCaseRows = [
+        // TODO: hardcoded data until API ready
+        {id: "1", subject: "my test case 1", description: "lorem ipsum dolores et", coverage: "95%"},
+        {id: "2", subject: "my test case 2", description: "lorem ipsum dolores et", coverage: "10%"}
+    ];
+
+    const testCaseLibraryRows = [
+        // TODO: hardcoded data until API ready
+        {id: 1, subject: 'Test case 1', description: 'This is a detail description of the test case'},
+        {id: 2, subject: 'Test case 2', description: 'This is a detail description of the test case'},
+        {id: 3, subject: 'Test case 3', description: 'This is a detail description of the test case'},
+        {id: 4, subject: 'Test case 4', description: 'This is a detail description of the test case'},
+        {id: 5, subject: 'Test case 5', description: 'This is a detail description of the test case'},
+        {id: 6, subject: 'Test case 6', description: 'This is a detail description of the test case'},
+        {id: 7, subject: 'Test case 7', description: 'This is a detail description of the test case'}
+    ];
+
+    const testCaseColumns = [
+    { field: 'id', headerName: 'ID', headerClassName: 'header', flex: 1},
+    { field: 'subject', headerName: 'Subject', headerClassName: 'header', flex: 1},
+    { field: 'description', headerName: 'Description', headerClassName: 'header', flex: 2},
+    { field: 'coverage', headerName: 'Coverage', headerClassName: 'header', flex: 1}, //calculated from tests
+    // { field: 'topology', headerName: 'Coverage', headerClassName: 'header', flex: 1}, 
+    // { field: 'configs', headerName: 'Device Configs', headerClassName: 'header', flex: 2},
+    { 
+        field: 'button', 
+        headerName: 'Actions',
+        headerClassName: 'header',
+        align: 'center',
+        renderCell: () => (
+        <p >
+            <HorizontalButton text="Edit"/>
+            <HorizontalButton text="View Details"/>
+            <HorizontalButton text="Delete"/>
+        </p>
+        ),
+        flex: 1
+    }
+    ];
+
+    const BOMRows = [
+        // TODO: hardcoded data until API ready
+        {id: "1", deviceName: "Router", optional: "False", quantity: "50", physicalOrSoftware: "Physical", codeVer: "none", SKU: "323123"},
+        {id: "2", deviceName: "Router", optional: "False", quantity: "50", physicalOrSoftware: "Physical", codeVer: "none", SKU: "323123"},
+        {id: "3", deviceName: "Router", optional: "False", quantity: "50", physicalOrSoftware: "Physical", codeVer: "none", SKU: "323123"},
+        {id: "4", deviceName: "Router", optional: "False", quantity: "50", physicalOrSoftware: "Physical", codeVer: "none", SKU: "323123"},
+        {id: "5", deviceName: "Router", optional: "False", quantity: "50", physicalOrSoftware: "Physical", codeVer: "none", SKU: "323123"},
+        {id: "6", deviceName: "Router", optional: "False", quantity: "50", physicalOrSoftware: "Physical", codeVer: "none", SKU: "323123"},
+        {id: "7", deviceName: "Router", optional: "False", quantity: "50", physicalOrSoftware: "Physical", codeVer: "none", SKU: "323123"},
+        {id: "8", deviceName: "Router", optional: "False", quantity: "50", physicalOrSoftware: "Physical", codeVer: "none", SKU: "323123"},
+        {id: "9", deviceName: "Router", optional: "False", quantity: "50", physicalOrSoftware: "Physical", codeVer: "none", SKU: "323123"},
+    ];
+
+    const BOMColumns = [
+        { field: 'id', headerName: 'ID', headerClassName: 'header', flex: 1},
+        { field: 'deviceName', headerName: 'Device Name', headerClassName: 'header', flex: 1},
+        { field: 'optional', headerName: 'Optional', headerClassName: 'header', flex: 1},
+        { field: 'quantity', headerName: 'Quantity', headerClassName: 'header', flex: 1},
+        { field: 'physicalOrSoftware', headerName: 'Physical/Software', headerClassName: 'header', flex: 1},
+        { field: 'codeVer', headerName: 'Code Version', headerClassName: 'header', flex: 1},
+        { field: 'SKU', headerName: 'SKU', headerClassName: 'header', flex: 1},
+        { 
+            field: 'button', 
+            headerName: 'Actions',
+            headerClassName: 'header',
+            align: 'center',
+            renderCell: () => (
+            <p >
+                <HorizontalButton text="Edit"/>
+                <HorizontalButton text="Delete"/>
+            </p>
+            ),
+            flex: 1
+        }
+    ];
+
+
+    function testCases() {
+        // Test plans table component
+        return (
+            <div className={styles.tableContainer} style={{paddingTop: 50}}>
+                <div className={styles.tableButtonRow}>
+                    <h2>Test Cases of Current Plan</h2>
+                    <CPButton text="Add New"
+                            onClick={() => {updateModal("select");
+                                    console.log(selectModalOpen);
+                                }}
+                    />
+                </div>
+                <PlainTable rows={testCaseRows} columns={testCaseColumns} className={classes.root}/>
+            </div>
+        )
+    }
+
+    function BOMSummary() {
+        // Summary of BOM Elements component
+        return (
+            <div className={styles.tableContainer} style={{paddingTop: 50}}>
+                <div className={styles.tableButtonRow}>
+                    <h2>Summary of Bill of Materials</h2>
+                    <CPButton text="Add New"/>
+                </div>
+                <PlainTable rows={BOMRows} columns={BOMColumns} className={classes.root}/>
+            </div>
+        )
+    }
+
+
+
+    const [selectModalOpen, setSelectModalOpen] = useState(false);
+    const [infoModalOpen, setInfoModalOpen] = useState(false);
+    const [cloneModalOpen, setCloneModalOpen] = useState(false);
+    const emptyRow = {subject: '', description: ''};
+    const [selectedRow, setSelectedRow] = useState(emptyRow);   
+  
+    
+    function updateModal(modalType,rowId=0){
+      // console.log(input);
+      // setModalType(input);
+      console.log("modalOpen called with ");
+      console.log(modalType);
+      if (modalType === "select"){
+        setSelectModalOpen(true)
+      } else if (modalType === "scratch"){
+        setSelectedRow(emptyRow);
+        setInfoModalOpen(true)
+      } else if (modalType === "clone"){
+        setCloneModalOpen(true)
+      } else if (modalType === "clone_selected"){
+        setCloneModalOpen(false)
+        setInfoModalOpen(true)
+        const selectedRowData = (testCaseLibraryRows.filter((row) => rowId===row.id))[0];
+        setSelectedRow(selectedRowData);
+      }
+      else {
+        setSelectModalOpen(false)
+        setInfoModalOpen(false)
+        setCloneModalOpen(false)
+      }
+    }
+
+    
+    function description() {
+        return (
+            <div style={{display: "flex", flexDirection: "column"}}>
+                <p>Subject: </p>
+                <p>Active: (Boolean)</p>
+                <p>Details: </p>
+                <p>Device Config: </p>
+                <p>Coverage: </p>
+                <p>Version: </p>
+                <p>Date Created: </p>
+                <p>Authors: </p>
+                <p>Customer feedback</p>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <NewTestCaseModal
+              modalOpen={selectModalOpen} 
+              onClickNext={updateModal}
+              onClose={()=> setSelectModalOpen(false)}></NewTestCaseModal>
+
+            <NewTestCaseModalInfo
+              modalOpen={infoModalOpen} 
+              onBack={()=> setInfoModalOpen(false)}
+              selectedRow={selectedRow}
+              ></NewTestCaseModalInfo>
+
+            <NewTestCaseModalClone
+              modalOpen={cloneModalOpen} 
+              onClickNext={updateModal}
+              onBack={()=> setCloneModalOpen(false)}
+              ></NewTestCaseModalClone>
+        
+        <SplitScreen
+            topChildren={<h1>Test Plan Details</h1>}
+            leftSection={description()}
+            // rightSection={testCaseTimeEstimates()}
+            bottomChildren={
+                <div>
+                {testCases()}
+                {BOMSummary()}
+                </div>
+            }
+        />
+        </div> 
+ 
+    )
+}
---
+++ cradlepoint/pages/testCaseModals/newCaseModalInfo.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Modal from 'react-modal';
+import {CPButton} from "../../components/button/CPButton";
+import styles from '../../styles/Modal.module.css'
+import { SmallTextInput, BigTextInput } from "../../components/fields/Text";
+import { borderLeft } from "@mui/system";
+
+export default function NewTestCaseModalInfo(props) {
+  return (
+    <>
+      <Modal className={styles.Modal} isOpen={props.modalOpen}>
+        <h2>Fill in New Test Case Info</h2>
+        <div style={{alignItems:borderLeft}}>
+        <SmallTextInput name='Subject' value={props.selectedRow.subject}/>
+        <BigTextInput name='Description' value={props.selectedRow.description}/>
+        </div>
+        <CPButton text='Back' onClick={props.onBack}/>
+        <CPButton text='Create'/>
+      </Modal>
+    </>
+  );
+}
