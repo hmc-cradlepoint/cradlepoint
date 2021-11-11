fb0a6b4e05f688ea340018d2ec840c83a425b75c
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-11-10 16:17:42 -0800

    adds Login Authentication

--- cradlepoint/components/baseScreen/MenuBar.jsx
+++ cradlepoint/components/baseScreen/MenuBar.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import useUser from '../../util/useUser';
+import { useRouter } from 'next/router';
+import fetchJson from '../../util/fetchJson';
+
 
 export default function MenuBar() {
-    return (
+  const { user, mutateUser } = useUser();
+  const router = useRouter();
+  return (
     // TODO: add navigation
     <Stack direction="row" spacing={4} padding={2} borderBottom="1px solid">
       <Button >Home</Button>
       <Button>Test Plan Library</Button>
       <Button>Device Library</Button>
       <Button>Notifications</Button>
-      <Button>Logout</Button>
+      {user?.isLoggedIn && (
+        <Button onClick={async (e) => {
+          e.preventDefault()
+          mutateUser( await fetchJson('/api/logout', { method: 'POST' }),
+                      false)
+          router.push('/login')
+        }}>Logout</Button>
+      )}
       <Button>Help</Button>
     </Stack>
     )
--- cradlepoint/pages/api/getActiveEngagements.js
+++ cradlepoint/pages/api/getActiveEngagements.js
@@ -6,9 +6,8 @@ export default async (req, res) => {
   // All status codes greater than 13 are for inactive engagements
   const query = { statusCode: { $lt: 13 } };
 
-  const client = await connectToDb();
-  const cursor = await client.collection("engagements").find(query);
-  
+  const db = await connectToDb();
+  const cursor = db.collection("engagements").find(query);
   const results = await cursor.toArray();
   res.json(results);
 };
