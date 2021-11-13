d1c46d17e42923c72abc754e3d8032de125a6dbd
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-11-13 15:05:13 -0800

    Example Validates

--- cradlepoint/schemas/engagementSchema.js
+++ cradlepoint/schemas/engagementSchema.js
@@ -2,6 +2,7 @@ import * as yup from 'yup';
 const { ObjectId } = require('bson');
 
 export const engagementSchema = yup.object().shape({
+  _id: yup.string().optional(),
   statusCode: yup.number().positive().integer().required(),
   engagementDetails: yup.string().required(),
   SE: yup.string().matches(/^\d+$/),
@@ -9,7 +10,7 @@ export const engagementSchema = yup.object().shape({
   customer: yup.string().required(),
   SFDC: yup.string().url(),
   BOM: yup.array().required(),
-  testplanID: yup.string().required(),
+  testPlanId: yup.string().required(),
   createdOn: yup.date().default(function () {
     return new Date();
   }).required(),
