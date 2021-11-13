e4b2da1db3a5169920f512057d153e45f55f9ee7
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-11-13 10:35:10 -0800

    Broken ObjectID Validation

--- cradlepoint/schemas/engagementSchema.js
+++ cradlepoint/schemas/engagementSchema.js
@@ -1,4 +1,24 @@
 import * as yup from 'yup';
+const { ObjectId } = require('bson');
+
+// See: https://github.com/jquense/yup/issues/467
+class ObjectIdSchema extends yup.mixed {
+  constructor() {
+    super({ type: 'objectId' });
+
+    this.withMutation(schema => {
+      schema.transform(function(value) {
+        if (this.isType(value)) return value;
+        return new ObjectId(value);
+      });
+    });
+  }
+  
+  _typeCheck(value) {
+    return ObjectId.isValid(value);
+  }
+}
+yup.ObjectId = () => new ObjectIdSchema();
 
 export const engagementSchema = yup.object().shape({
   statusCode: yup.number().positive().integer().required(),
@@ -8,6 +28,7 @@ export const engagementSchema = yup.object().shape({
   customer: yup.string().required(),
   SFDC: yup.string().url(),
   BOM: yup.array().required(),
+  testplanID: yup.ObjectId.required(),
   createdOn: yup.date().default(function () {
     return new Date();
   }).required(),
