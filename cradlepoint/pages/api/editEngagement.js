a2f405eb76458f777f87c58f7b5b1d8206481e98
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-11-16 04:19:22 -0800

    Update editEngagement.js

--- cradlepoint/pages/api/editEngagement.js
+++ cradlepoint/pages/api/editEngagement.js
@@ -6,22 +6,23 @@ import {engagementSchema} from "../../schemas/engagementSchema";
 */
 // const query = { '_id': ObjectId("617249b199915be1b771aca1") };
 // const data = {
-//   '_id': "617249b199915be1b771aca1", 
-//   'statusCode': 2,
-//   'engagementDetails': 'Test Details 2',
-//   'SE': '1234',
-//   'POC_Engineer': '2345',
-//   'customer': 'Big Finance',
-//   'SFDC': 'https://cradlepoint.lightning.force.com/lightning/r/Opportunity/006380...',
-//   'BOM': [""],
-//   'testPlanId': "61724e5599915be1b771acb2",
-//   'createdOn': new Date(2021, 10, 20, 7, 0, 0, 0)
+//   "_id": "617249b199915be1b771aca1", 
+//   "statusCode": 2,
+//   "engagementDetails": "Test Details 2",
+//   "SE": "1234",
+//   "POC_Engineer": "2345",
+//   "customer": "Big Finance",
+//   "SFDC": "https://cradlepoint.lightning.force.com/lightning/r/Opportunity/006380...",
+//   "BOM": [""],
+//   "testPlanId": "61724e5599915be1b771acb2",
+//   "createdOn": "2021-11-20T15:00:00.000Z"
+//   //new Date(2021, 10, 20, 7, 0, 0, 0)
 // }; 
 
 export default async (req, res) => {
-  if (req.method !== 'POST') {
-    res.status(405).send({ message: 'Only POST requests allowed' })
-  }
+  // if (req.method !== 'POST') {
+  //   res.status(405).send({ message: 'Only POST requests allowed' })
+  // }
   try{
     const data = req.body;
     const valid = await engagementSchema.isValid(data);
@@ -30,12 +31,14 @@ export default async (req, res) => {
       // Set ID strings to Mongo ObjectId's
       const newId = ObjectId(result._id);
       const newTestPlanId = ObjectId(result.testPlanId);
+      const query = {_id: newId};
       delete result._id;
       delete result.testPlanId;
       const engagement = {_id: newId, testPlanId:newTestPlanId, ...result };
+      console.log(engagement);
       // Update the Database w/ new Engagement
       const db = await connectToDb();
-      await db.collection("engagements").replaceOne(newId, engagement);
+      await db.collection("engagements").replaceOne(query, engagement);
       //res.json(engagement);
       res.status(200).send({message: "Success!"});
     } else {
