f4328942051dbb22aa336c9432532f31811fd1d1
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-11-16 05:15:17 -0800

    Only Commit

--- cradlepoint/schemas/testSchema.js
+++ cradlepoint/schemas/testSchema.js
@@ -1,6 +1,7 @@
 import * as yup from 'yup';
 
 export const testSchema = yup.object().shape({
+  _id: yup.string().optional(),
   details: yup.string().required(),
   name: yup.string().required(),
   results: yup.array().required(),
