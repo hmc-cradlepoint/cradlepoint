c8777176de90e709372d8a9c8140606d28c6ed75
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-11-16 10:13:40 -0800

    Update editTestPlan.js

--- cradlepoint/pages/api/editTestPlan.js
+++ cradlepoint/pages/api/editTestPlan.js
@@ -4,22 +4,10 @@ import {testPlanSchema} from "../../schemas/testPlanSchema";
 /*
   Edits the requested TestPlan from the database
 */
-// const data = {
-//   "_id": "6172500699915be1b771acb3", 
-//   "name": "LTE Failover",
-//   "detailedDescription": "Test failover to LTE during upstream failure",
-//   "version": "1234",
-//   "deviceConfig": "[{'system':{'system_id':'LTE Router'}}, []]",
-//   "customerFeedback": "Works as expected",
-//   "testCases": [""],
-//   "authors": [""],
-//   "isActive": false,
-//   "createdOn": "2021-10-21T07:00:00.000Z"
-// }; 
 
 export default async (req, res) => {
-  if (req.method !== 'POST') {
-    res.status(405).send({ message: 'Only POST requests allowed' })
+  if (req.method !== 'PUT') {
+    res.status(405).send({ message: 'Only PUT requests allowed' })
   }
   try{
     const data = req.body;
@@ -34,14 +22,11 @@ export default async (req, res) => {
       // Update the Database w/ new TestPlan
       const db = await connectToDb();
       await db.collection("testPlan").replaceOne(query, testPlan);
-      //res.json(testPlan);
       res.status(200).send({message: "Success!"});
     } else {
       res.status(422).send({message: 'Input not in right format'})
     }
-
   } catch (err) {
-    console.log(err);
     res.status(500).send(err);
   }
 };
