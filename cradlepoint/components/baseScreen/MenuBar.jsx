696b2f3cbdb9f64b1075d87e2c416543c2d060c0
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-11-20 11:19:33 -0800

    Removing Auth Changes

--- cradlepoint/components/baseScreen/MenuBar.jsx
+++ cradlepoint/components/baseScreen/MenuBar.jsx
@@ -1,29 +1,16 @@
 import React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import useUser from '../../util/useUser';
-import { useRouter } from 'next/router';
-import fetchJson from '../../util/fetchJson';
-
 
 export default function MenuBar() {
-  const { user, mutateUser } = useUser();
-  const router = useRouter();
-  return (
+    return (
     // TODO: add navigation
     <Stack direction="row" spacing={4} padding={2} borderBottom="1px solid">
       <Button >Home</Button>
       <Button>Test Plan Library</Button>
       <Button>Device Library</Button>
       <Button>Notifications</Button>
-      {user?.isLoggedIn && (
-        <Button onClick={async (e) => {
-          e.preventDefault()
-          mutateUser( await fetchJson('/api/logout', { method: 'POST' }),
-                      false)
-          router.push('/login')
-        }}>Logout</Button>
-      )}
+      <Button>Logout</Button>
       <Button>Help</Button>
     </Stack>
     )
