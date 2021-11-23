5f8e903733674c2876065319b43c357151d39649
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-11-23 07:13:17 -0800

    strange error

--- cradlepoint/schemas/testCaseSchema.js
+++ cradlepoint/schemas/testCaseSchema.js
@@ -1,6 +1,7 @@
 import * as yup from 'yup';
 
 export const testCaseSchema = yup.object().shape({
+  _id: yup.string().optional(),
   timeEstimate: yup.number().positive().required(),
   description: yup.string().required(),
   config: yup.string().required(),
