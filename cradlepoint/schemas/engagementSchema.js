c70cdbaedbfea77865201eecd8299d2dfacf07ad
mtran-12 <60111775+mtran-12@users.noreply.github.com>
2021-11-30 10:15:13 -0800

    updated schema to fit frontend

--- cradlepoint/schemas/engagementSchema.js
+++ cradlepoint/schemas/engagementSchema.js
@@ -1,6 +1,7 @@
 import * as yup from 'yup';
 
 export const engagementSchema = yup.object().shape({
+  name: yup.string().required(),
   statusCode: yup.number().positive().integer().required(),
   engagementDetails: yup.string().required(),
   SE: yup.string().matches(/^\d+$/),
