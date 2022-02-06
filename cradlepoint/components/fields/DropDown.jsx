89f566d56872b6332f0fd5586ecf75d0a3b92942
Jkwok2018 <jessica.kwok19@gmail.com>
2022-02-05 18:30:18 -0800

    dropdown component done

--- cradlepoint/components/fields/DropDown.jsx
+++ cradlepoint/components/fields/DropDown.jsx
@@ -5,24 +5,22 @@ import { Field, Formik} from 'formik';
 
 // 1-line text field
 export default function DropDown(props) {
-    console.log(props.options);
+
+    function handleChange(evt) {
+      console.log(evt);
+      props.onChange(evt);
+    }
+    
     return (
         <div style={{padding: "25px"}}>
         <Formik>
           <label>
             {props.title}
-            <Field as="select" name={props.fieldName} value={props.value} onChange={props.handleChange}>
-              {/* <option value="pass">Pass</option>
-              <option value="unknown">Unknown</option>
-              <option value="fail">Fail</option> */}
-              {/* TODO: this is just displaying blank */}
-              {props.options?.map(({ value }) => (
-               
-                <option value={value}>
-                  {value}
-                </option>
-              
-                ))}
+            <Field as="select" name={props.fieldName} value={props.value} 
+            onChange={handleChange}>
+              {props.options.map((o) => {
+                return <option value={o}>{o}</option>
+              })}
             </Field>
             </label>
           </Formik>
