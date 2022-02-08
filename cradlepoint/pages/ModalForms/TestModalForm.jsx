c8465e12568b6b10ad3e6aef0bbcada08ad966c9
Jkwok2018 <jessica.kwok19@gmail.com>
2022-02-07 23:30:13 -0800

    call different endpoints based on modalFormType

--- cradlepoint/pages/ModalForms/TestModalForm.jsx
+++ cradlepoint/pages/ModalForms/TestModalForm.jsx
@@ -6,6 +6,7 @@ import { SmallTextInput, BigTextInput } from "../../components/fields/Text";
 import { borderLeft } from "@mui/system";
 import { useRouter } from 'next/router'
 import {ObjectID} from 'bson';
+import {modalFormType} from '../../util/modalUtils';
 
 export default function TestModalForm(props) {
   const router = useRouter();
@@ -29,20 +30,35 @@ export default function TestModalForm(props) {
       ...props.data, 
       "_id":data._id, 
       "name":data.name, 
-      "description":data.description
+      "description":data.description,
+      "testCaseId": props.testCaseId,
+      "results": []
     }
+
+    const endPoint = '/api/editTest';
+    const method = 'PUT';
+    if (props.modalFormType==modalFormType.NEW){
+      endPoint = '/api/addNewTest';
+      method = 'POST';
+    }
+
     try{
-      const res = await fetch('/api/editTest', {
-        method: 'PUT',
+      const d = JSON.stringify(newData);
+      const res = await fetch(endPoint, {
+        method: method,
         headers: {
           'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(d)
         },
         body: JSON.stringify(newData),
       })
+      console.log(res);
     } catch (err){
       console.log("Error:",err)
     }
-    props.onBack()
+    // TODO: if create new, then should navigate to the corresponding test details page
+    props.onBack();
+    setData(initialData);
   }
 
   return (
