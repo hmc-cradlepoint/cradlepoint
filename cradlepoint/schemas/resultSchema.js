1daa441bc05786fa809f4202ee3d3a0bd7d28ad8
McGarvs <47725552+McGarvs@users.noreply.github.com>
2022-02-13 21:30:02 -0800

    Result Field Reordering

--- cradlepoint/schemas/resultSchema.js
+++ cradlepoint/schemas/resultSchema.js
@@ -6,8 +6,8 @@ export const resultSchema = yup.object().shape({
   // POCApproval: yup.string().matches(/^\d+$/),
   // SEApproval: yup.string().matches(/^\d+$/),
   evidence: yup.string().optional(),
-  testId: yup.string().required(),
   resultStatus: yup.string().required(),
+  testId: yup.string().required(),
   createdOn: yup.date().default(function () {
     return new Date();
   }).required()
