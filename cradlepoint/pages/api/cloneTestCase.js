803cdce9a3fb01ad1ab47192de45cc0bb89d38bf
Jkwok2018 <jessica.kwok19@gmail.com>
2022-02-26 22:58:40 -0800

    uncommented error catching

--- cradlepoint/pages/api/cloneTestCase.js
+++ cradlepoint/pages/api/cloneTestCase.js
@@ -5,11 +5,11 @@ export default async function handler(req, res) {
       res.status(405).send({ message: 'Only POST requests allowed' })
     }
     else {
-      // try {
+      try {
         const response = await cloneTestCase(req.body)
         res.status(200).send({message: response});
-      // } catch (err) {
-      //   res.status(500).send(err.message);
-      // }
+      } catch (err) {
+        res.status(500).send(err.message);
+      }
     }
 }
