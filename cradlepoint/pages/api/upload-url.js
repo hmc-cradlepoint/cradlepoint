8d892a9b059a13e8efb861bf837553d1c6c5c983
mtran-12 <60111775+mtran-12@users.noreply.github.com>
2022-02-28 14:50:54 -0800

    Changed how env file is use to work with Mac (hopefully)

--- cradlepoint/pages/api/upload-url.js
+++ cradlepoint/pages/api/upload-url.js
@@ -2,9 +2,9 @@ import aws from 'aws-sdk';
 
 export default async function handler(req, res) {
   aws.config.update({
-    accessKeyId: process.env.ACCESS_KEY,
-    secretAccessKey: process.env.SECRET_KEY,
-    region: process.env.REGION,
+    accessKeyId: `${process.env.ACCESS_KEY}`,
+    secretAccessKey: `${process.env.SECRET_KEY}`,
+    region: `${process.env.REGION}`,
     signatureVersion: 'v4',
   });
 
