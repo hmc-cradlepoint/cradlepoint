373531d5aca0c13c52987be1ffae88e333855289
mtran-12 <60111775+mtran-12@users.noreply.github.com>
2022-02-28 19:23:40 -0800

    changed file structure

--- cradlepoint/schemas/deviceSchema.js
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup';
-
-export const deviceSchema = yup.object().shape({
-  _id: yup.string().optional(),
-  deviceName: yup.string().required(),
-  codeVersion: yup.string().required(),
-  SKU: yup.string().required(),
-  deviceType: yup.string().required(),
-});
