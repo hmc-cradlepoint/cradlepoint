94f2eaad76603dab0b1b8b64a6d2747145254603
mtran-12 <60111775+mtran-12@users.noreply.github.com>
2022-02-28 19:49:10 -0800

    fixed pages

--- cradlepoint/pages/ResultModalForm.jsx
+++ cradlepoint/pages/ResultModalForm.jsx
@@ -15,7 +15,7 @@ export default function ResultModalForm(props) {
       _id: props.data?._id??new ObjectID(),
       description: props.data?.description??"",
       evidence: props.data?.evidence??"",
-      testId: props.data.testId,
+      testId: props.data?.testId,
       resultStatus: props.data?.resultStatus??"Unknown"
     }
     
@@ -82,4 +82,3 @@ ResultModalForm.propTypes = {
   onBack: PropTypes.bool.isRequired,
   isOpen: PropTypes.bool.isRequired,
 }
-
