bd95df63f2b082a7ee4f8841618fdd00cf9a2cc2
mtran-12 <60111775+mtran-12@users.noreply.github.com>
2022-02-28 17:05:23 -0800

    fixed pages for aws

--- cradlepoint/pages/editModalFlow.jsx
+++ cradlepoint/pages/editModalFlow.jsx
@@ -9,41 +9,44 @@ import ResultModalForm from "./ResultModalForm";
 export default function EditModalFlow(props) {
 
   function getModal() {
-    switch (props.type) {
+    switch (props?.type) {
       case flowType.ENGAGEMENT:
         return <EngagementModalForm modalFormType={modalFormType.EDIT} 
-                                    data={props.data} 
-                                    isOpen={props.modalOpen} 
-                                    onBack={props.onClose}
-                                    onClose={props.onClose}/>
+                                    data={props?.data} 
+                                    isOpen={props?.modalOpen} 
+                                    onBack={props?.onClose}
+                                    onClose={props?.onClose}/>
       case flowType.TEST_PLAN:
         return <TestPlanModalForm modalFormType={modalFormType.EDIT} 
-                                  data={props.data} 
-                                  engagementId={props.data.engagementId}
-                                  isOpen={props.modalOpen} 
-                                  onBack={props.onClose}
-                                  onClose={props.onClose}/>
+                                  data={props?.data} 
+                                  engagementId={props?.data.engagementId}
+                                  isOpen={props?.modalOpen} 
+                                  onBack={props?.onClose}
+                                  onClose={props?.onClose}/>
       case flowType.TEST_CASE:
         return <TestCaseModalForm modalFormType={modalFormType.EDIT} 
-                                  data={props.data} 
-                                  testPlanId={props.data.testPlanId}
-                                  isOpen={props.modalOpen} 
-                                  onBack={props.onClose} 
-                                  onClose={props.onClose}/>
+                                  data={props?.data} 
+                                  testPlanId={props?.data.testPlanId}
+                                  isOpen={props?.modalOpen} 
+                                  onBack={props?.onClose} 
+                                  onClose={props?.onClose}/>
       case flowType.TEST:
         return <TestModalForm modalFormType={modalFormType.EDIT} 
-                              data={props.data} 
-                              testCaseId={props.data.testCaseId}
-                              isOpen={props.modalOpen} 
+                              data={props?.data} 
+                              testCaseId={props?.data.testCaseId}
+                              isOpen={props?.modalOpen} 
                               // Note that this is NOT duplicating because for createModalFlow onBack and onClose are different
                               // The TestModalForm will call both
-                              onBack={props.onClose} 
-                              onClose={props.onClose}/>
+                              onBack={props?.onClose} 
+                              onClose={props?.onClose}/>
       case flowType.RESULT:
         return <ResultModalForm modalFormType={modalFormType.EDIT} 
-                                data={props.data} 
-                                isOpen={props.modalOpen} 
-                                onBack={props.onClose} />
+                                data={props?.data} 
+                                isOpen={props?.modalOpen} 
+                                onBack={props?.onClose} />
+      default:
+        console.log('Not valid flow type')
+        return <div>Something went wrong here</div>
       }
   }
     return (
