f447933060de587617d8fd1fdc89805389182a87
Jkwok2018 <jessica.kwok19@gmail.com>
2022-03-26 10:26:22 -0700

    implemented editDeviceInBOM, not fully tested

---
+++ pages/api/editDeviceInBOM.js
@@ -0,0 +1,37 @@
+const { ObjectId } = require('mongodb');
+import connectToDb from "../../util/mongodb";
+import {bomDeviceSchema} from "../../schemas/bomDeviceSchema";
+/*
+  Edits the requested test from the database
+*/
+
+export default async (req, res) => {
+  if (req.method !== 'PUT') {
+    res.status(405).send({ message: 'Only PUT requests allowed' })
+  }
+  try{
+    const data = req.body;
+    
+    const client = await connectToDb();
+    if (ObjectId.isValid(data.testCaseId)) {
+        const testCaseId = ObjectId(data.testCaseId);
+        let device = data.devices[0];
+        const valid = await bomDeviceSchema.isValid(device);
+        if (valid){
+            device.deviceId = ObjectId(device.deviceId);
+            // Update the corresponding device of the BOM
+            const result = await client.collection('testCases').updateOne(
+                { "_id": testCaseId,  "BOM.deviceId" :  device.deviceId }, // query matching , refId should be "ObjectId" type
+                { $set :  {"BOM.$": device}},
+            );
+        } else{
+          res.status(422).send({message: "device not valid "});
+        }
+          res.status(200).send({message: "Success!"});
+        } else {
+          res.status(422).send({message: 'Input not in right format'});
+        }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
