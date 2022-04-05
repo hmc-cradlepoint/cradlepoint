9298e0205a21e34bfc9471bac7f04bf916649df0
mtran-12 <60111775+mtran-12@users.noreply.github.com>
2022-04-05 16:28:49 -0700

    Possibly fixed env vars

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_DB: process.env.MONGODB_DB,
+    HOST: process.env.HOST,
+    ACCESS_KEY: process.env.ACCESS_KEY,
+    SECRET_KEY: process.env.SECRET_KEY,
+    region: process.env.region,
+    BUCKET_NAME: process.env.BUCKET_NAME,
+  }, 
   reactStrictMode: true,
   webpack5: false,
   async redirects() {
